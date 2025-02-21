76f365599682717b1f1e79adae198f712987bf60
Bhavya Kothari <129605972+bakothari@users.noreply.github.com>
2025-02-21 10:37:24 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,3 @@
 //your JS code here. If required.
+let info = document.querySelector("#browser-info")
+info.textContent = `You are using ${navigator.userAgent} ${navigator.appName} version ${navigator.appVersion}.`
